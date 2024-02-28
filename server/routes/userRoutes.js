96851e1ae819e5be228cf396d7dbc51df0a57114
asnganje <abd.nganje@gmail.com>
2024-02-28 18:21:29 +0300

    pass login and signup logics to router

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const express = require('express');
+const { signup, login } = require('../controllers/authController');
 const router = express.Router()
 
-router.route('/signup').post()
-router.route('login').post()
+router.route('/signup').post(signup)
+router.route('login').post(login)
 
 module.exports = router;
