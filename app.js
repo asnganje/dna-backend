9fd878193148533b11961bcc12dac51c4201434f
asnganje <abd.nganje@gmail.com>
2024-02-28 18:34:03 +0300

    Add DB connection logic

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRoute = require('./server/routes/userRoutes')
+const connectDB = require('./server/db/connect')
 
 app.use(express.json())
 app.use('/api/v1/dna', userRoute)
 
 const port = process.env.PORT;
+const url = process.env.MONGO_URI;
 
 
-const start = () => {
+const start = async() => {
     try {
+        await connectDB(url)
         app.listen(port, ()=> {
             console.log(`Server is listening on port ${port}`);
         })
