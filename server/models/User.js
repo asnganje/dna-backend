e4141f7b95bc1c997cd131db05f9a7b260aeaba5
asnganje <abd.nganje@gmail.com>
2024-02-29 17:49:19 +0300

    modify model logic

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  nickName: {
     type: String,
     required: [true, "Please provide user name "],
+    unique: true
+  },
+  id: {
+    type: Number,
+    required: [true, "Please provide user id "],
+    unique: true
   },
   email: {
     type: String,
     required: [true, "Please provide user email "],
+    unique: true
+  },
+  phone: {
+    type: Number,
+    required: [true, "Please provide user phone number "],
+    unique: true
+  },
+  authenticationNumber: {
+    type: String,
+    default: null
   },
 });
 
